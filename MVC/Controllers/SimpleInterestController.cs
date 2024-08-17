54c78958aa0719a37844df9f5f165c75bf547f28
Sudharsanan-Grl <121048921+sudharsanan-velmurugan@users.noreply.github.com>
2024-08-18 00:16:24 +0530

    setting up

---
+++ MVC/Controllers/SimpleInterestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class SimpleInterestController : Controller
+    {
+        public IActionResult Index(SimpleInterestModel? si=null)
+        {
+            if(si==null)
+            {
+                si = new SimpleInterestModel()
+                {
+                    PrincipleAmount = 5000000,
+                    RateOfInterest = 16,
+                    YearsOfInvestment = 4,
+                };
+            }
+            
+            si.SimpleIntersest();
+
+            return View(si);
+        }
+    }
+}
---
+++ MVC/Models/SimpleInterestModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Models
+{
+    public class SimpleInterestModel 
+    {
+        public decimal? PrincipleAmount { get; set; }
+        public decimal? RateOfInterest { get; set; }
+        public decimal? YearsOfInvestment { get; set; }
+        public decimal?  SimpleInterest { get; set; }
+        
+        public SimpleInterestModel() { }
+        public void SimpleIntersest() {
+            SimpleInterest = (PrincipleAmount * RateOfInterest * YearsOfInvestment) / 100;
+        }
+    }
+}
